c888265e07523a092e3746c426464a81a38e3837
kuccho524 <kucchachachanocha@gmail.com>
2021-06-30 07:54:15 +0000

    Modify followings and followers order

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -18,12 +18,12 @@ class RelationshipsController < ApplicationController
   # フォロー一覧
   def followings
     user = User.find(params[:user_id])
-    @users = user.followings.page(params[:page]).per(10)
+    @users = user.followings.page(params[:page]).reverse_order.per(10)
   end
 
   # フォロワー一覧
   def followers
     user = User.find(params[:user_id])
-    @users = user.followers.page(params[:page]).per(10)
+    @users = user.followers.page(params[:page]).reverse_order.per(10)
   end
 end
